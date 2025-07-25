c309430f1e6bde3251ca0c731a8ec1da2c410283
ielkhalifa1 <122579851+ielkhalifa1@users.noreply.github.com>
2025-07-25 22:51:02 +0100

    pushing my app files

---
+++ verifyToken.js
@@ -0,0 +1,16 @@
+const jsonwebtoken = require("jsonwebtoken");
+
+function auth(req, res, next) {
+    const token = req.header('auth-token');
+    if (!token) return res.status(401).send('Access Denied');
+
+    try {
+        const verified = jsonwebtoken.verify(token, process.env.TOKEN_SECRET);
+        req.user = verified;
+        next();
+    } catch (err) {
+        res.status(400).send('Invalid Token');
+    }
+}   
+
+module.exports = auth
